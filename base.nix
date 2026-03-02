cdfd8f504415f2a4a8c3f406a6cd892722a1ef42
Rakandhiya Rachmanto <rakandhiya.daanii@gmail.com>
2026-03-02 15:00:09 -0500

    add postgres as common service to all nodes

--- base.nix
+++ base.nix
@@ -68,5 +68,18 @@
   # (1) tailscale
   services.tailscale.enable = true;
 
+  services.postgresql = {
+    enable = true;
+    ensureDatabases = [ "mydatabase" ];
+    enableTCPIP = true;
+    port = 5432;
+    authentication = pkgs.lib.mkOverride 10 ''
+      #type database DBuser  origin-address auth-method
+      local all      all     trust
+      host  all      all     127.0.0.1/32   trust
+      host  all      all     ::1/128        trust
+      '';
+  };
+
   system.stateVersion = "25.11";
 }
