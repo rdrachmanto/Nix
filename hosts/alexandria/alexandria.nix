3a90276c713e1dbaf5e02c64312f73ab779b7ed8
Rakandhiya Rachmanto <rakandhiya.daanii@gmail.com>
2026-06-01 00:51:32 -0400

    turn off samba temporarily

--- hosts/alexandria/alexandria.nix
+++ hosts/alexandria/alexandria.nix
@@ -16,38 +16,38 @@
   # Enable ssh into machine
   services.openssh.enable = true;
 
-  services.samba = {
-    enable = true;
-    securityType = "user";
-    openFirewall = true;
-    settings = {
-      global = {
-        "workgroup" = "WORKGROUP";
-        "server string" = "smbnix";
-        "netbios name" = "smbnix";
-        "security" = "user";
-        "hosts allow" = "127.0.0.1 100.";
-        "hosts deny" = "0.0.0.0/0";
-        "guest account" = "nobody";
-        "map to guest" = "bad user";
-      };
-      "public" = {
-        "path" = "/srv/Shares/Public";
-        "browseable" = "yes";
-        "read only" = "no";
-        "guest ok" = "yes";
-        "create mask" = "0644";
-        "directory mask" = "0755";
-        "force user" = "main";
-        "force group" = "users";
-      };
-    };
-  };
+  # services.samba = {
+  #   enable = true;
+  #   securityType = "user";
+  #   openFirewall = true;
+  #   settings = {
+  #     global = {
+  #       "workgroup" = "WORKGROUP";
+  #       "server string" = "smbnix";
+  #       "netbios name" = "smbnix";
+  #       "security" = "user";
+  #       "hosts allow" = "127.0.0.1 100.";
+  #       "hosts deny" = "0.0.0.0/0";
+  #       "guest account" = "nobody";
+  #       "map to guest" = "bad user";
+  #     };
+  #     "public" = {
+  #       "path" = "/srv/Shares/Public";
+  #       "browseable" = "yes";
+  #       "read only" = "no";
+  #       "guest ok" = "yes";
+  #       "create mask" = "0644";
+  #       "directory mask" = "0755";
+  #       "force user" = "main";
+  #       "force group" = "users";
+  #     };
+  #   };
+  # };
 
-  services.samba-wsdd = {
-    enable = true;
-    openFirewall = true;
-  };
+  # services.samba-wsdd = {
+  #   enable = true;
+  #   openFirewall = true;
+  # };
 
   networking.firewall.allowedTCPPorts = [];
   networking.firewall.trustedInterfaces = [ "tailscale0" ];
